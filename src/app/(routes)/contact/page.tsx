7be4288b69b85668dcbd30492772a50effc7f103
Subhane00 <elibeyovasubhane7@gmail.com>
2026-05-02 00:05:34 +0400

    v0.0.2

--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -4,53 +4,54 @@ const Contact = () => {
       <div className="contact">
         
         <div className="contact-info">
-
+        <p><b>Contact Info</b></p>
           <div className="info-div">
             <i className="fa-solid fa-location-dot"></i>
             <p>
-              Head Office 187 Rogers dr <br />
-              STOUGHTON, MA 02072 <br />
-              United States
+              Head Office 187 <br />
+              
+             <span style={{color: ' #E5CDA6'}}> United States</span>
             </p>
           </div>
 
           <div className="info-div">
-            <i className="fa-solid fa-location-dot"></i>
+           <i className="fa-brands fa-weixin"></i>
             <p>
-              Head Office 187 Rogers dr <br />
-              STOUGHTON, MA 02072 <br />
-              United States
+              Chat with us<br />
+            <span style={{color: ' #E5CDA6'}}>We are here 24/7 to help</span><br />
+             
             </p>
           </div>
 
           <div className="info-div">
-            <i className="fa-solid fa-location-dot"></i>
+            <i className="fa-solid fa-phone"></i>
             <p>
-              Head Office 187 Rogers dr <br />
-              STOUGHTON, MA 02072 <br />
-              United States
+              Hotliner <br />
+            <span style={{color: ' #E5CDA6'}}>Mon - Fri: 8am - 4pm</span><br />
             </p>
           </div>
 
         </div>
 
         <div className="contact-message">
+          <p><b>Leave a Message</b></p>
+          <p>We'd love to hear from you. Our friendly team is always heere to help you.</p>
             <form action="#">
                 <div className="name-box">
                     <p>Your Name</p>
-                    <input type="text" />
+                    <input type="text" style={{width:'500px',height:'33px', backgroundColor:'#F5F5F5', border:'none'}} />
                 </div>
                   <div className="name-box">
                     <p>Your Email</p>
-                    <input type="text" />
+                    <input type="text" style={{width:'500px',height:'33px', backgroundColor:'#F5F5F5', border:'none'}}/>
                 </div>
                   <div className="name-box">
                     <p>Subject</p>
-                    <input type="text" />
+                    <input type="text" style={{width:'500px',height:'33px', backgroundColor:'#F5F5F5', border:'none'}} />
                 </div>
                 <div className="name-box">
                     <p>Your message (optional) </p>
-                    <textarea></textarea>
+                    <textarea style={{width:'500px',height:'80px', backgroundColor:'#F5F5F5', border:'none'}}></textarea>
                 </div>
             </form>
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { usePathname } from 'next/navigation'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import '../assets/sass/style.scss';
+import '../assets/sass/_contact.scss';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -29,7 +30,7 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         /></head>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
